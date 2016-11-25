eb1703ec135b4c51ecaf618ad20ab5213f452858
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2016-11-25 21:49:08 +0100

    Add banner
    Signed-off-by: Drasko DRASKOVIC <drasko.draskovic@gmail.com>

--- main.go
+++ main.go
@@ -55,5 +55,30 @@ func main() {
 	cache.Start(cfg.CacheURL())
 	defer cache.Stop()
 
+	// Print banner
+	fmt.Println(banner)
+	fmt.Println("Magic happens on port " + httpPort)
+
 	http.ListenAndServe(httpPort, api.Server())
 }
+
+var banner = `
+ __    __     ______     __     __   __     ______   __         __  __     __  __    
+/\ "-./  \   /\  __ \   /\ \   /\ "-.\ \   /\  ___\ /\ \       /\ \/\ \   /\_\_\_\   
+\ \ \-./\ \  \ \  __ \  \ \ \  \ \ \-.  \  \ \  __\ \ \ \____  \ \ \_\ \  \/_/\_\/_  
+ \ \_\ \ \_\  \ \_\ \_\  \ \_\  \ \_\\"\_\  \ \_\    \ \_____\  \ \_____\   /\_\/\_\ 
+  \/_/  \/_/   \/_/\/_/   \/_/   \/_/ \/_/   \/_/     \/_____/   \/_____/   \/_/\/_/ 
+                                                                                     
+ ______     __  __     ______   __  __                                               
+/\  __ \   /\ \/\ \   /\__  _\ /\ \_\ \                                              
+\ \  __ \  \ \ \_\ \  \/_/\ \/ \ \  __ \                                             
+ \ \_\ \_\  \ \_____\    \ \_\  \ \_\ \_\                                            
+  \/_/\/_/   \/_____/     \/_/   \/_/\/_/  
+
+                       == Relax, everything's locked ==
+
+                        Made with <3 by Mainflux Team
+[w] http://mainflux.io
+[t] @mainflux
+
+`
